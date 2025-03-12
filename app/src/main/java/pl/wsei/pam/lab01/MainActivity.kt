edd8b3f4beb9b5095670c485d6e6cea0d51938d7
wiktoria.zygmunt <wiktoria.zygmunt@>
2025-03-12 12:36:41 +0100

    working

--- app/src/main/java/pl/wsei/pam/lab01/MainActivity.kt
+++ app/src/main/java/pl/wsei/pam/lab01/MainActivity.kt
@@ -12,7 +12,7 @@ import androidx.appcompat.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     lateinit var mLayout: LinearLayout
     lateinit var mTitle: TextView
-    private lateinit var mProgress: ProgressBar
+    lateinit var mProgress: ProgressBar
     var mBoxes: MutableList<CheckBox> = mutableListOf()
     var mButtons: MutableList<Button> = mutableListOf()
 
@@ -72,78 +72,79 @@ class MainActivity : AppCompatActivity() {
             var isTestPassed = false
 
             button.setOnClickListener({
-                when (i) {
-                    1 -> if (
-                        task11(4, 6) in 0.666665..0.666667 &&
-                        task11(7, -6) in -1.1666667..-1.1666665
-                    ) {
-                        mBoxes[0].isChecked = true
-                        isTestPassed = true
+                if (!isTestPassed) {
+                    when (i) {
+                        1 -> if (
+                            task11(4, 6) in 0.666665..0.666667 &&
+                            task11(7, -6) in -1.1666667..-1.1666665
+                        ) {
+                            mBoxes[0].isChecked = true
+                            isTestPassed = true
+                        }
+
+                        2 -> if (
+                            task12(7U, 6U) == "7 + 6 = 13" &&
+                            task12(12U, 15U) == "12 + 15 = 27"
+                        ) {
+                            mBoxes[1].isChecked = true
+                            isTestPassed = true
+                        }
+
+                        3 -> if (
+                            task13(0.0, 5.4f) && !task13(7.0, 5.4f) &&
+                            !task13(-6.0, -1.0f) && task13(6.0, 9.1f) &&
+                            !task13(6.0, -1.0f) && task13(1.0, 1.1f)
+                        ) {
+                            mBoxes[2].isChecked = true
+                            isTestPassed = true
+                        }
+
+                        4 -> if (
+                            task14(-2, 5) == "-2 + 5 = 3" &&
+                            task14(-2, -5) == "-2 - 5 = -7"
+                        ) {
+                            mBoxes[3].isChecked = true
+                            isTestPassed = true
+                        }
+
+                        5 -> if (
+                            task15("DOBRY") == 4 &&
+                            task15("barDzo dobry") == 5 &&
+                            task15("doStateczny") == 3 &&
+                            task15("Dopuszczający") == 2 &&
+                            task15("NIEDOSTATECZNY") == 1 &&
+                            task15("XYZ") == -1
+                        ) {
+                            mBoxes[4].isChecked = true
+                            isTestPassed = true
+                        }
+
+                        6 -> if (task16(
+                                mapOf("A" to 2U, "B" to 4U, "C" to 3U),
+                                mapOf("A" to 1U, "B" to 2U)
+                            ) == 2U
+                            &&
+                            task16(
+                                mapOf("A" to 2U, "B" to 4U, "C" to 3U),
+                                mapOf("F" to 1U, "G" to 2U)
+                            ) == 0U
+                            &&
+                            task16(
+                                mapOf("A" to 23U, "B" to 47U, "C" to 30U),
+                                mapOf("A" to 1U, "B" to 2U, "C" to 4U)
+                            ) == 7U
+                        ) {
+                            mBoxes[5].isChecked = true
+                            isTestPassed = true
+                        }
                     }
 
-                    2 -> if (
-                        task12(7U, 6U) == "7 + 6 = 13" &&
-                        task12(12U, 15U) == "12 + 15 = 27"
-                    ) {
-                        mBoxes[1].isChecked = true
-                        isTestPassed = true
+
+                    if (isTestPassed) {
+                        val completedTasks = mBoxes.count { it.isChecked }
+                        val newProgress = (completedTasks.toFloat() / 6 * 100).toInt()
+                        mProgress.progress = newProgress
                     }
-
-                    3 -> if (
-                        task13(0.0, 5.4f) && !task13(7.0, 5.4f) &&
-                        !task13(-6.0, -1.0f) && task13(6.0, 9.1f) &&
-                        !task13(6.0, -1.0f) && task13(1.0, 1.1f)
-                    ) {
-                        mBoxes[2].isChecked = true
-                        isTestPassed = true
-                    }
-
-                    4 -> if (
-                        task14(-2, 5) == "-2 + 5 = 3" &&
-                        task14(-2, -5) == "-2 - 5 = -7"
-                    ) {
-                        mBoxes[3].isChecked = true
-                        isTestPassed = true
-                    }
-
-                    5 -> if (
-                        task15("DOBRY") == 4 &&
-                        task15("barDzo dobry") == 5 &&
-                        task15("doStateczny") == 3 &&
-                        task15("Dopuszczający") == 2 &&
-                        task15("NIEDOSTATECZNY") == 1 &&
-                        task15("XYZ") == -1
-                    ) {
-                        mBoxes[4].isChecked = true
-                        isTestPassed = true
-                    }
-
-                    6 -> if (task16(
-                            mapOf("A" to 2U, "B" to 4U, "C" to 3U),
-                            mapOf("A" to 1U, "B" to 2U)
-                        ) == 2U
-                        &&
-                        task16(
-                            mapOf("A" to 2U, "B" to 4U, "C" to 3U),
-                            mapOf("F" to 1U, "G" to 2U)
-                        ) == 0U
-                        &&
-                        task16(
-                            mapOf("A" to 23U, "B" to 47U, "C" to 30U),
-                            mapOf("A" to 1U, "B" to 2U, "C" to 4U)
-                        ) == 7U
-                    ) {
-                        mBoxes[5].isChecked = true
-                        isTestPassed = true
-                    }
-                }
-
-
-                if (isTestPassed) {
-                    val progressIncrement = 100 / 6
-                    val currentProgress = mProgress.progress
-                    val newProgress = currentProgress + progressIncrement
-                    mProgress.progress = if (newProgress > 100) 100 else newProgress
                 }
             })
         }
